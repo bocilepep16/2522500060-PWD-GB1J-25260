2747c41d883a117653dead860fbfce5534e26422
Catherine Audreylia Diony <2522500060@mahasiwa.atmaluhur.ac.id>
2025-12-18 17:26:17 +0700

    menambahkan penampil flash sukses maupun flash error menggunakan session pada read.php

--- pertemuan-12/README.md
+++ pertemuan-12/README.md
@@ -7,4 +7,5 @@ Hari Ini, Kamis 18 Desember 2025, Saya Mempelajari:
 <ol>
   <li>Copy isi file folder pertemuan 11 ke pertemuan 12</li>
   <li>menambah tombol Edit di read.php</li>
+  <li>menambahkan penampil flash sukses maupun flash error menggunakan session pada read.php</li>
 </ol>
--- pertemuan-12/read.php
+++ pertemuan-12/read.php
@@ -1,10 +1,35 @@
 <?php
-require 'koneksi.php';
+    session_start();
+    require 'koneksi.php';
+    require 'fungsi.php';
 
-$sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
-$q = mysqli_query($conn, $sql);
-$no = 1; #WAJIB ADA DI SINI
+    $sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
+    $q = mysqli_query($conn, $sql);
+    if (!$q) {
+        die("Query error: " . mysqli_error($conn));
+    }
 ?>
+
+<?php
+    $flash_sukses = $_SESSION['flash_sukses'] ?? ''; #jika query sukses
+    $flash_error  = $_SESSION['flash_error'] ?? '';  #jika ada error
+    #bersihkan session ini
+    unset($_SESSION['flash_sukses'], $_SESSION['flash_error']);
+?>
+
+<?php if (!empty($flash_sukses)) : ?>
+        <div style="padding:10px; margin-bottom:10px;
+            background:#d4edda; color:#155724; border-radius:6px;">
+            <?= $flash_sukses; ?>
+        </div>
+<?php endif; ?>
+
+<?php if (!empty($flash_error)) : ?>
+        <div style="padding:10px; margin-bottom:10px;
+            background:#f8d7da; color:#721c24; border-radius:6px;">
+            <?= $flash_error; ?>
+        </div>
+<?php endif; ?>
 
 <table border="1" cellpadding="8" cellspacing="0">
 <tr>
@@ -16,10 +41,10 @@ $no = 1; #WAJIB ADA DI SINI
     <th>Pesan</th>
     <th>Created At</th>
 </tr>
-
+<?php $i = 1; ?>
 <?php while ($row = mysqli_fetch_assoc($q)): ?>
 <tr>
-    <td><?= $no++; ?></td>
+    <td><?= $i++; ?></td>
     <td><a href= "edit.php?cid=<? (int)$row['cid']; ?>">Edit</a></td>
     <td><?= $row['cid']; ?></td>
     <td><?= htmlspecialchars($row['cnama']); ?></td>
